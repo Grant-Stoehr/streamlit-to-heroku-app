8121e198c9f398526b13c231d0313ad415355ba9
Grant-Stoehr <54612499+Grant-Stoehr@users.noreply.github.com>
2022-04-13 16:44:16 -0500

    Add files via upload
    Updated app with commenting

--- app.py
+++ app.py
@@ -2,6 +2,9 @@ import streamlit as st
 import time
 import numpy as np
 import pandas as pd
+import sqlite3
+from sqlite3 import Error
+
 
 df = pd.read_csv("WakeCountyHousing.csv")
 #df['Date'] = pd.to_datetime(df['Date'])
@@ -36,11 +39,38 @@ if st.button('Say hello'):
     st.session_state.count += 1
 
 
-your_city = st.text_input("Enter your city name")
-st.title(your_city)
+st.subheader('Enter your name')
+your_name = st.text_input("")
+st.title(your_name)
+
+conn = sqlite3.connect('app.sqlite')
+cur = conn.cursor()
+cur.execute("CREATE TABLE IF NOT EXISTS comments (comment varChar(350))")
+conn.close()
+
+#Create a text area
+st.subheader('Give us some feedback!')
+comment = st.text_area("")
+#push the comment to a db witht he press of a streamlit button
+if st.button('Submit'):
+    conn = sqlite3.connect('app.sqlite')
+    cur = conn.cursor()
+    cur.execute("INSERT INTO comments VALUES (?)", (comment,))
+    conn.commit()
+    conn.close()
+    
+
+st.subheader('Comments')
+conn = sqlite3.connect('app.sqlite')
+cur = conn.cursor()
+cur.execute("SELECT * FROM comments")
+comments = cur.fetchall()
+conn.close()
+st.table(comments)
 
 
 #if result:
 #    st.write(result)
 #    new_df = get_price_arount_date(st.session_state.chart)
 #    st.line_chart(new_df)
+
